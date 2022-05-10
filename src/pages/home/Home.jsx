4a6a1c63bbefe9cda3d4e89b43a9e0d200ad56fd
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-05-10 17:11:23 +0500

    Changing codes, deleting unnecessary files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import "./home.css"
 
 function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Header />
     </div>
   )
 }
