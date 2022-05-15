285f2b5b84472490d103b14c6929e9fc0e61509d
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-05-15 16:06:40 +0500

    Creating and Finishing propertyList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import "./home.css"
 
 function Home() {
@@ -12,6 +13,8 @@ function Home() {
       <div className='homeContainer'>
         <Featured />
         <h1 className='homeTitle'>Browse by property type</h1>
+        <PropertyList />
+        <h1>Home guests love</h1>
       </div>
     </div>
   )
