2436bb3aad7559e2ec5a0c2e39b6c477f6fa8974
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-05-15 17:47:10 +0500

    Doing state management in List Page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
+import { format } from 'date-fns'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import "./list.css"
 
 function List() {
+  const location = useLocation()
+  const [destination, setDestination] = useState(location.state.destination)
+  const [date, setDate] = useState(location.state.date)
+  const [options, setOptions] = useState(location.state.options)
+  
   return (
     <div>
       <Navbar />
       <Header type="list"/>
+      <div className="listContainer">
+        <div className='listWrapper'>
+          <div className="listSearch">
+            <h1 className='lsTitle'>Search</h1>
+            <div className='lsItem'>
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
+            </div>
+            <div className='lsItem'>
+              <label>Check-in-date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+            </div>
+          </div>
+          <div className='listResult'>
 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
