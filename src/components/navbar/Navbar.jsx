f8943f981544179bec11b17315547cf07d9c67c5
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-03 14:47:56 +0500

    Changing some of styles of logo in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import "./navbar.css"
 
 const Navbar = () => {
+  const navigation = useNavigate();
+
   return (
     <div className='navbar'>
       <div className="navContainer">
-        <span className="logo">uabooking</span>
+        <span onClick={()=> navigation("/")} className="logo">uabooking</span>
         <div className='navItems'>
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
